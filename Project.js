f129f8dd126e0d588361332c5b1a04999aa91baf
MSibley1 <sibleymontez@gmail.com>
2022-12-13 22:22:45 -0500

    New Images, Menu JS, and edited existing files

--- Project.js
+++ Project.js
@@ -53,16 +53,78 @@ function signIn(e) {
 // Management JS
 function delUserData() {
     localStorage.clear()
+    alert("All prior customer data has been deleted.")
 }
 
-function pop(){
-    console.log("test")
-}
-
-function disableCaliItem() {
-    if (document.getElementsByClassName("CaliBtn").disabled = true){
-        document.getElementsByClassName("CaliBtn").disabled = false;
+var manInfo = [
+    {
+        username:"Montez",
+        password: "1234"
+    },
+    {
+        username:"Abrya",
+        password:"1234"
     }
-    document.getElementsByClassName("CaliBtn").disabled = true;
+]
 
+
+function getInfo() {
+    var username = document.getElementById("username").value
+    var password = document.getElementById("password").value
+
+    for(i = 0; i < manInfo.length; i++){
+        if(username == manInfo[i].username && password == manInfo[i].password)
+        {
+            window.location.href = "Manage.html"
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+// Landing JS
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex+= n);
+}
+
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n){
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+    for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+// Cart JS
+
